479166383f2f3f379671f314f82ed98a7d62c04d
1rapeem <premvee.k@gmail.com>
2026-04-09 15:05:01 +0700

    edit env

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
 
   app.use(cookieParser());
   app.enableCors({
-    origin: 'http://localhost:5173', 
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173', 
     credentials: true, 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   });
