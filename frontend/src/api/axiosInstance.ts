f3e9e8bd89babda0e94d4fe2d5e5abb2a0de1ad5
1rapeem <premvee.k@gmail.com>
2026-04-09 16:47:39 +0700

    edit path

--- frontend/src/api/axiosInstance.ts
+++ frontend/src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.BACKEND_URL, 
+  baseURL: process.env.VITE_BACKEND_URL, 
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
